c7b8c2beb6f50b022009033895ed21ff68d58e53
Olga Chovancova <chovancova.olga@yahoo.com>
2017-03-25 00:14:20 +0100

    Autoservice generator class

--- SimpleEventSimulation/AutoserviceLibrary/AutoserviceGenerators.cs
+++ SimpleEventSimulation/AutoserviceLibrary/AutoserviceGenerators.cs
@@ -1,18 +1,127 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RandomGenerators;
+﻿using RandomGenerators;
+using RandomGenerators.Generators;
 
 namespace AutoserviceLibrary
 {
-   public  class AutoserviceGenerators
+    public class AutoserviceGenerators
     {
+        private readonly ExponencionalDistribution _gen1;
+        private readonly DiscreteEmpiricalDistribution _gen2;
+        private readonly UniformContinuousDistribution _gen3;
+        private readonly UniformContinuousDistribution _gen4;
+        private readonly TriangleUniformDistribution _gen5;
+        private readonly UniformContinuousDistribution _gen6;
+        private readonly DiscreteEmpiricalDistribution _gen7;
+
         public AutoserviceGenerators()
         {
-            GeneratorSeed seed = new GeneratorSeed();
-            
+            var seed = new GeneratorSeed();
+
+            _gen1 = new ExponencionalDistribution(seed.GetRandomSeed(), 300);
+            _gen2 = new DiscreteEmpiricalDistribution(seed.GetRandomSeed(), new[]
+            {
+                new Duration(1, 1, 0.4), new Duration(2, 2, 0.15), new Duration(3, 3, 0.14), new Duration(4, 4, 0.12),
+                new Duration(5, 5, 0.1), new Duration(6, 6, 0.09)
+            });
+
+            _gen3 = new UniformContinuousDistribution(seed.GetRandomSeed(), 70, 310);
+            _gen4 = new UniformContinuousDistribution(seed.GetRandomSeed(), 80, 160);
+            _gen5 = new TriangleUniformDistribution(seed.GetRandomSeed(), 120, 540, 240);
+            _gen6 = new UniformContinuousDistribution(seed.GetRandomSeed() + seed.GetRandomSeed(), 123, 257);
+            _gen7 = new DiscreteEmpiricalDistribution(seed.GetRandomSeed(), new[]
+            {
+                new Duration(0, 0, 0.7), new Duration(0, 0, 0.2), new Duration(0, 0, 0.1)
+            }, new IGenerators[]
+            {
+                new UniformDiscreteDistribution(seed.GetRandomSeed(), 2, 20),
+                new DiscreteEmpiricalDistribution(seed.GetRandomSeed(),
+                    new[] {new Duration(10, 40, 0.1), new Duration(41, 61, 0.6), new Duration(62, 100, 0.3)}),
+                new UniformDiscreteDistribution(seed.GetRandomSeed(), 120, 260)
+            });
+        }
+
+        /// <summary>
+        ///     prud zakaznikov prichadzaujucich do autoservisu je poissonovsky prud s intenzitou
+        ///     z == 12 zakaznikov za hodinu
+        ///     modelujem exp.rozdelenim  300 s
+        /// </summary>
+        /// <returns></returns>
+        public double Generator1()
+        {
+            return _gen1.GenerateDouble();
+        }
+
+        /// <summary>
+        ///     Pravdepodobnosti poctu oprav, tkore bude zakaznik pozadovat
+        ///     pocet oprav       1       2       3       4       5       6
+        ///     pravdepodobnst    0.4     0.15    0.14    0.12    0.1     0.09
+        ///     Empiricke rozdelenie
+        /// </summary>
+        /// <returns></returns>
+        public int Generator2()
+        {
+            return _gen2.GenerateInt();
+        }
+
+        /// <summary>
+        ///     Cas potrebny na prevzatie objednavky od zakaznika
+        ///     o = 190 s +- 120 s
+        ///     Spojite rovnomerne - 70, 310
+        /// </summary>
+        /// <returns></returns>
+        public double Generator3()
+        {
+            return _gen3.GenerateDouble();
+        }
+
+        /// <summary>
+        ///     cas potrebny na prevzatie auta od zakaznika
+        ///     p = 120s +- 40 s
+        ///     diskretne uniform = 80, 160
+        /// </summary>
+        /// <param name="seed"></param>
+        public double Generator4()
+        {
+            return _gen4.GenerateDouble();
+        }
+
+        /// <summary>
+        ///     Preparkovanie auta z parkoviska do dielne alebo naspat ssa riadi
+        ///     trojuholnikovym rozdelenim s parametrami
+        ///     min = 120 s, max 540, a modus = 240 s
+        /// </summary>
+        /// <param name="seed"></param>
+        public double Generator5()
+        {
+            return _gen5.GenerateDouble();
+        }
+
+        /// <summary>
+        ///     Prevzatie opraveneho auta trva s = 190 s +- 67s
+        ///     diskretne uniform = <123, 257>
+        /// </summary>
+        /// <param name="seed"></param>
+        public double Generator6()
+        {
+            return _gen6.GenerateDouble();
+        }
+
+        /// <summary>
+        ///     /////////////////////////////////////////////////
+        ///     pravdepodobnosti jednotlivych oprav a ich trvanie
+        ///     JEDNODUCHA OPRAVA - 0.7
+        ///     diskretne rovnomerne = Tmin = 2, Tmax = 20
+        ///     STREDNE TAZKA OPRAVA - 0.2
+        ///     diskretne empiricke
+        ///     T:   10-40       41-61       62-100
+        ///     p:   0.1         0.6         0.3
+        ///     ZLOZITA OPRAVA
+        ///     diskretne rovnomerne Tmin = 120, Tmax = 260
+        /// </summary>
+        /// <returns></returns>
+        public int Generator7()
+        {
+            return _gen7.GenerateInt();
         }
     }
 }
--- SimpleEventSimulation/TestGenerators/Program.cs
+++ SimpleEventSimulation/TestGenerators/Program.cs
@@ -1,42 +1,38 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 using RandomGenerators;
 using RandomGenerators.Generators;
 
 namespace TestGenerators
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             TestGenerators();
-
         }
 
         public static void TestGenerators()
         {
             //SIMULACNY CAS SU SEKUNDY
-            GeneratorSeed seed = new GeneratorSeed();
-            //generator1(seed);
+            var seed = new GeneratorSeed();
+            generator1(seed);
 
-            //generator2(seed);
+            generator2(seed);
 
-            //generator3(seed);
+            generator3(seed);
 
-            //generator4(seed);
+            generator4(seed);
 
-            //generator5(seed);
+            generator5(seed);
 
-            //generator6(seed);
+            generator6(seed);
 
-            //generator7(seed);
+            generator7(seed);
 
-            //generator8(seed);
+            generator8(seed);
 
-            //generator9(seed);
+            generator9(seed);
 
             generator10(seed);
         }
@@ -48,7 +44,7 @@ namespace TestGenerators
             {
                 new Duration(0, 0, 0.7), new Duration(0, 0, 0.2), new Duration(0, 0, 0.1)
             };
-            IGenerators[] generators = new IGenerators[]
+            IGenerators[] generators =
             {
                 new UniformDiscreteDistribution(seed.GetRandomSeed(), 2, 20),
                 new DiscreteEmpiricalDistribution(seed.GetRandomSeed(),
@@ -56,7 +52,7 @@ namespace TestGenerators
                 new UniformDiscreteDistribution(seed.GetRandomSeed(), 120, 260)
             };
 
-            DiscreteEmpiricalDistribution gen10 = new DiscreteEmpiricalDistribution(seed.GetRandomSeed(), d3, generators);
+            var gen10 = new DiscreteEmpiricalDistribution(seed.GetRandomSeed(), d3, generators);
             TestDistributionsToFileInt(gen10, "generator_10_empiricke.dst");
         }
 
@@ -64,7 +60,7 @@ namespace TestGenerators
         {
             //ZLOZITA OPRAVA
             //diskretne rovnomerne Tmin = 120, Tmax = 260
-            UniformDiscreteDistribution gen9 = new UniformDiscreteDistribution(seed.GetRandomSeed(), 120, 260);
+            var gen9 = new UniformDiscreteDistribution(seed.GetRandomSeed(), 120, 260);
             TestDistributionsToFileInt(gen9, "generator_9_diskretne_uniform_min_120_max_260.dst");
         }
 
@@ -78,7 +74,7 @@ namespace TestGenerators
             {
                 new Duration(10, 40, 0.1), new Duration(41, 61, 0.6), new Duration(62, 100, 0.3)
             };
-            DiscreteEmpiricalDistribution gen8 = new DiscreteEmpiricalDistribution(seed.GetRandomSeed(), d2);
+            var gen8 = new DiscreteEmpiricalDistribution(seed.GetRandomSeed(), d2);
             TestDistributionsToFileInt(gen8, "generator_8_empiricke.dst");
         }
 
@@ -88,7 +84,7 @@ namespace TestGenerators
             //pravdepodobnosti jednotlivych oprav a ich trvanie
             // JEDNODUCHA OPRAVA
             //diskretne rovnomerne = Tmin = 2, Tmax = 20
-            UniformDiscreteDistribution gen7 = new UniformDiscreteDistribution(seed.GetRandomSeed(), 2, 20);
+            var gen7 = new UniformDiscreteDistribution(seed.GetRandomSeed(), 2, 20);
             TestDistributionsToFileInt(gen7, "generator_7_diskretne_uniform_min_2_max_20.dst");
         }
 
@@ -96,17 +92,17 @@ namespace TestGenerators
         {
             //prevzatie opraveneho auta trva s = 190 s +- 67s
             //diskretne uniform = <123, 257>
-            UniformContinuousDistribution gen6 = new UniformContinuousDistribution(seed.GetRandomSeed() + seed.GetRandomSeed(), 123,
+            var gen6 = new UniformContinuousDistribution(seed.GetRandomSeed() + seed.GetRandomSeed(), 123,
                 257);
             TestDistributionsToFileDouble(gen6, "generator_6_spojite_uniform_min_123_max_257.dst");
         }
-      
+
         private static void generator5(GeneratorSeed seed)
         {
             //preparkovanie auta z parkoviska do dielne alebo naspat ssa riadi 
             //trojuholnikovym rozdelenim s parametrami 
             //min = 120 s, max 540, a modus = 240 s
-            TriangleUniformDistribution gen5 = new TriangleUniformDistribution(seed.GetRandomSeed(), 120, 540, 240);
+            var gen5 = new TriangleUniformDistribution(seed.GetRandomSeed(), 120, 540, 240);
             TestDistributionsToFileDouble(gen5, "generator_5_trojholnikove_min_120_max_540_modus_240.dst");
         }
 
@@ -115,33 +111,32 @@ namespace TestGenerators
             //cas potrebny na prevzatie auta od zakaznika 
             //p = 120s +- 40 s
             // diskretne uniform = <80, 160> 
-            UniformContinuousDistribution gen4 = new UniformContinuousDistribution(seed.GetRandomSeed(), 80, 160);
+            var gen4 = new UniformContinuousDistribution(seed.GetRandomSeed(), 80, 160);
             TestDistributionsToFileDouble(gen4, "generator_4_spojite_uniform_min_80_max_160.dst");
-
         }
 
-   
+
         private static void generator3(GeneratorSeed seed)
         {
             //Cas potrebny na prevzatie objednavky od zakaznika
             //o = 190 s +- 120 s  
             //Spojite rovnomerne - <70, 310>
-            UniformContinuousDistribution gen3 = new UniformContinuousDistribution(seed.GetRandomSeed(), 70, 310);
+            var gen3 = new UniformContinuousDistribution(seed.GetRandomSeed(), 70, 310);
             TestDistributionsToFileDouble(gen3, "generator_3_spojite_uniform_min_70_max_310.dst");
         }
-  
+
         private static void generator2(GeneratorSeed seed)
         {
             //Pravdepodobnosti poctu oprav, tkore bude zakaznik pozadovat
             //pocet oprav       1       2       3       4       5       6
             //pravdepodobnst    0.4     0.15    0.14    0.12    0.1     0.09
             //Empiricke rozdelenie
-            Duration[] d = new[]
+            Duration[] d =
             {
                 new Duration(1, 1, 0.4), new Duration(2, 2, 0.15), new Duration(3, 3, 0.14), new Duration(4, 4, 0.12),
                 new Duration(5, 5, 0.1), new Duration(6, 6, 0.09)
             };
-            DiscreteEmpiricalDistribution discPocet_oprav = new DiscreteEmpiricalDistribution(seed.GetRandomSeed(), d);
+            var discPocet_oprav = new DiscreteEmpiricalDistribution(seed.GetRandomSeed(), d);
             TestDistributionsToFileInt(discPocet_oprav, "generator_2_empiricke.dst");
         }
 
@@ -150,39 +145,33 @@ namespace TestGenerators
             //prud zakaznikov prichadzaujucich do autoservisu je poissonovsky prud s intenzitou 
             //z == 12 zakaznikov za hodinu
             //modelujem exp.rozdelenim => 300 s 
-            ExponencionalDistribution ex300 = new ExponencionalDistribution(seed.GetRandomSeed(), 300);
+            var ex300 = new ExponencionalDistribution(seed.GetRandomSeed(), 300);
             TestDistributionsToFileDouble(ex300, "generator_1_exp_300.dst");
         }
 
 
-
-        static void TestDistributionsToFileDouble(IGenerators generator, string filename = "data.dst")
+        private static void TestDistributionsToFileDouble(IGenerators generator, string filename = "data.dst")
         {
-            int numbers = 1000000;
-            using (System.IO.StreamWriter file =
-           new System.IO.StreamWriter(@filename))
+            var numbers = 1000000;
+            using (var file =
+                new StreamWriter(filename))
             {
-                for (int i = 0; i < numbers; i++)
-                {
-                   file.Write((generator.GenerateDouble().ToString().Replace(',', '.')) + "\n");
-                }
+                for (var i = 0; i < numbers; i++)
+                    file.Write(generator.GenerateDouble().ToString().Replace(',', '.') + "\n");
             }
             Console.WriteLine(filename);
         }
-        static void TestDistributionsToFileInt(IGenerators generator, string filename = "data.dst")
+
+        private static void TestDistributionsToFileInt(IGenerators generator, string filename = "data.dst")
         {
-            int numbers = 1000000;
-            using (System.IO.StreamWriter file =
-           new System.IO.StreamWriter(filename))
+            var numbers = 1000000;
+            using (var file =
+                new StreamWriter(filename))
             {
-                for (int i = 0; i < numbers; i++)
-                {
-                    file.Write(generator.GenerateInt().ToString() + "\n");
-                }
+                for (var i = 0; i < numbers; i++)
+                    file.Write(generator.GenerateInt() + "\n");
             }
             Console.WriteLine(filename);
-
         }
-
     }
 }
