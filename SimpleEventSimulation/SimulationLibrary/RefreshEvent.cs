3a858f2417e3c70e7653a032a1003a83c4d64338
olga.chovancova <chovancova.olga@yahoo.com>
2017-03-29 14:12:29 +0200

    refresh event

---
+++ SimpleEventSimulation/SimulationLibrary/RefreshEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SimulationLibrary
+{
+    class RefreshEvent : SimEvent
+    {
+        public RefreshEvent(double eventTime, SimCore simulation) : base(eventTime, simulation)
+        {
+        }
+        /***
+         * Refresh event is always carried out. 
+         **/
+        public override void Execute()
+        {
+            double refresh = EventTime + ReferenceSimCore.RefreshRate;
+            RefreshEvent refreshEvent = new RefreshEvent(refresh, ReferenceSimCore);
+            ReferenceSimCore.ScheduleEvent(refreshEvent, refresh);
+
+            Thread.Sleep((int) Math.Round(ReferenceSimCore.SleepingTime));
+
+        }
+    }
+}
--- SimpleEventSimulation/SimulationLibrary/SimCore.cs
+++ SimpleEventSimulation/SimulationLibrary/SimCore.cs
@@ -8,13 +8,15 @@ namespace SimulationLibrary
     {
         public double CurrentTime { get; set; }
         private SimplePriorityQueue<SimEvent, double> _timeLine;
-
+        public bool IsEnabledObserverMode { get; set; }
         public IGenerators[] Generators { get; set; }
         public virtual event EventHandler Refresh;
         public bool IsRunning { get; set; }
         public double MaximumSimulationTime { get; set; }
         private int _numberOfStatistic = 4;
         public double[] Result { get; set; }
+        public double RefreshRate { get; set; }
+        public double SleepingTime { get; set; }
 
         public SimCore(IGenerators[] generators, double maxTime)
         {
